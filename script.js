7b849ed9b0a0c87011fd874ab87ee9ff7495fd2b
fire-bro5056 <mishaleshcenko2002@gmail.com>
2025-11-22 12:54:20 -0800

    mm

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.menu_link') 
+const sections = document.querySelectorAll('section')
+let active = document.querySelector('.active')
+window.addEventListener('scroll',()=>{
+    const position =window.scrollY
+    console.log(position)
+})
