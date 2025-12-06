c96302d3c095043e1a68d21052b38e469e441d73
fire-bro5056 <mishaleshcenko2002@gmail.com>
2025-12-06 12:20:25 -0800

    hh

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const buttons = document.querySelectorAll('.menu_link')
 const sections = document.querySelectorAll('section')
 let active = document.querySelector('.active')
 window.addEventListener('scroll',()=>{
-    const position =window.scrollY
-    console.log(position)
+    const currentSection=getCurrentSection()
+    console.log(currentSection)
 })
+function getCurrentSection(){
+    const position =window.scrollY
+    for(let section of sections){
+        const sectionTop= section.offsetTop
+        const sectionBottom= section.offsetTop+ section.offsetHeight
+        if (position>=sectionTop&&position<sectionBottom){
+             return section
+
+        }
+        
+            
+
+    }
+}
